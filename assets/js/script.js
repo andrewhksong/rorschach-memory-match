0ee20868a06406ffa9374c6983328f9f00677cdc
andrewhksong <andrewhksong@gmail.com>
2020-04-18 23:14:30 -0700

    User can reset game and play again

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ let firstCardClicked
 let secondCardClicked
 let firstCardClasses
 let secondCardClasses
-let maxMatches = 9
+let maxMatches = 2
 let matches = 0
+let attempts = 0
+let gamesPlayed = 0
 
 gameCards.addEventListener('click', handleClick)
 
@@ -18,34 +20,24 @@ function handleClick(event) {
         firstCardClicked = event.target
         const cardClass = firstCardClicked.previousElementSibling.className;
         firstCardClasses = cardClass.slice(cardClass.length - 3, cardClass.length)
-        console.log('1st class:', firstCardClasses);
     } else {
         secondCardClicked = event.target
         const cardClass = secondCardClicked.previousElementSibling.className;
         secondCardClasses = cardClass.slice(cardClass.length - 3, cardClass.length)
-        console.log('2nd class:', secondCardClasses);
         gameCards.removeEventListener('click', handleClick)
         if(firstCardClasses === secondCardClasses) {
             matches++
-            console.log('matches:', matches)
+            attempts++
+            displayStats()
             if(matches === maxMatches) {
-                let body = document.body
-                let modalOverlay = document.createElement('div')
-                let modalContent = document.createElement('div')
-                let modalText = document.createElement('p')
-                modalOverlay.className = 'modal-overlay'
-                modalContent.className = 'modal-content'
-                modalText.textContent = `You've Won!`
-                body.prepend(modalOverlay)
-                modalOverlay.prepend(modalContent)
-                modalContent.prepend(modalText)
+                resetGame()
             }
             firstCardClicked = null
             secondCardClicked = null
             gameCards.addEventListener('click', handleClick)
-            console.log(firstCardClicked, secondCardClicked)
-            console.log('success')
         } else {
+            attempts++
+            displayStats()
             gameCards.removeEventListener('click', handleClick)
             setTimeout(() => {
                 firstCardClicked.classList.remove('hidden')
@@ -54,7 +46,57 @@ function handleClick(event) {
                 secondCardClicked = null
                 gameCards.addEventListener('click', handleClick)
             }, 1500)
-            console.log(firstCardClicked, secondCardClicked)
         }
     }
 }
+
+function displayStats() {
+    let gamesPlayedStats = document.querySelector('#games-played')
+    let attemptsStats = document.querySelector('#attempts')
+    let accuracyStats = document.querySelector('#accuracy')
+    attemptsStats.textContent = attempts
+    accuracyStats.textContent = calculateAccuracy(attempts, matches)
+    gamesPlayedStats.textContent = gamesPlayed
+}
+
+function calculateAccuracy(attempts, matches) {
+    let percentage = Math.trunc((matches / attempts) * 100)
+    if(!percentage) {
+        return `0%`
+    }
+    return `${percentage}%`
+}
+
+function resetGame() {
+    let body = document.body
+    let modalOverlay = document.createElement('div')
+    let modalContent = document.createElement('div')
+    let modalText = document.createElement('p')
+    let resetButton = document.createElement('button')
+    modalOverlay.className = 'modal-overlay'
+    modalContent.className = 'modal-content'
+    resetButton.className = 'modal-button'
+    modalText.textContent = `You've Won!`
+    resetButton.textContent = 'Start A New Challenge'
+    body.prepend(modalOverlay)
+    modalOverlay.prepend(modalContent)
+    modalContent.prepend(modalText)
+    modalContent.prepend(resetButton)
+    resetButton.addEventListener('click', () => {
+        $(".modal-overlay").remove()
+    });
+    matches = 0;
+    attempts = 0;
+    gamesPlayed++;
+    resetCards();
+    displayStats();
+}
+
+function resetCards() {
+    var hiddenCards = document.querySelectorAll('.card-back');
+    for (var listItem = 0; listItem < hiddenCards.length; listItem++) {
+        hiddenCards[listItem].classList.remove('hidden');
+    }
+}
+
+
