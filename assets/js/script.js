a67b5a73775b9260060625bac482b3fcca4c0939
andrewhksong <andrewhksong@gmail.com>
2020-04-23 23:36:27 -0700

    Adjusted reset button container

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let firstCardClicked
 let secondCardClicked
 let firstCardClasses
 let secondCardClasses
-let maxMatches = 9
+let maxMatches = 1
 let matches = 0
 let attempts = 0
 let gamesPlayed = 0
